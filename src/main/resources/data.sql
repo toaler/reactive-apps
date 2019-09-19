8c8da90aa4c2893c26818dc2d4416f2ce5651a0c
Brian Toal <btoal@salesforce.com>
2019-09-18 22:42:34 -0700

    Initial Revision

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO CITY (ID, NAME, STATE, COUNTRY, MAP) values (2000, 'Washington', 'DC', 'US', 'Google');
+INSERT INTO CITY (ID, NAME, STATE, COUNTRY, MAP) values (2001, 'Boston', 'MA', 'US', 'Google');
